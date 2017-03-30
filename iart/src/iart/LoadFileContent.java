2c19354072bbcf001658db3cfc552c30b4f2c2a1
Zombobilium <vcjonny@gmail.com>
2017-03-30 01:02:25 +0100

    read_edges, read_nodes from files and add_node and add_edge done

--- iart/src/iart/LoadFileContent.java
+++ iart/src/iart/LoadFileContent.java
@@ -2,7 +2,13 @@ package iart;
 
 public class LoadFileContent {
 	
-	public LoadFileContent(){
+	public LoadFileContent(){}
+	
+	public void read_nodes(){
+		
+	}
+	
+	public void read_edges(){
 		
 	}
 
---
+++ iart/src/iart/RoadMap.java
@@ -0,0 +1,30 @@
+package iart;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.implementations.SingleGraph;
+
+public class RoadMap {
+
+	Graph graph = new SingleGraph("RoadMap");
+
+
+	public RoadMap(){
+		
+		LoadFileContent filesContent = new LoadFileContent();
+		filesContent.read_nodes();
+		filesContent.read_edges();
+
+		graph.display();
+	}
+	
+	public void add_node(String id){
+		Node node = graph.addNode(id);
+	}
+	
+	public void add_edge(String id, String firstNodeId, String secondNodeId){
+		Edge edge = graph.addEdge(id, firstNodeId, secondNodeId);
+	}
+
+}
